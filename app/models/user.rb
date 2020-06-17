bda5ff1ce8118494e7596ebce73cedc5e85fce0a
benoit barges <benoitbarges@gmail.com>
2020-06-17 19:24:40 +0200

    users migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :email, uniqueness: true
 end

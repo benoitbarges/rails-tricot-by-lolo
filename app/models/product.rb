5c24dcb0700bbcd378d8e972dd21b924e44f88dc
benoit barges <benoitbarges@gmail.com>
2020-06-18 17:53:35 +0200

    productsController setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :category
+  CATEGORY = ["Pull", "Bonnet", "Mitaines", "Châle", "Gilet", "Chaussons", "Snood", "Col"].sort
+
   has_many :order_products
-  has_one_attached :photos
+  has_one_attached :photo
 
   validates :name, :price, :size, :description, :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -5,11 +5,31 @@ class ProductPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
   def new?
     user.admin
   end
 
   def create?
+    user.admin
+  end
+
+  def edit?
+    user.admin
+  end
+
+  def update?
+    user.admin
+  end
+
+  def destroy?
     user.admin
   end
 end

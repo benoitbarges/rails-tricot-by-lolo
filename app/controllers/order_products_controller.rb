b5c01003fd1ced815b18432e0e7fbc1469b6193f
benoit barges <benoitbarges@gmail.com>
2020-06-28 18:46:54 +0200

    add create route

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,2 +1,12 @@
 class OrderProductsController < ApplicationController
+
+  def create
+    @order_product = OrderProduct.new
+  end
+
+  private
+
+  def order_product_params
+    params.require(:order_products).permit(:order_id, :product_id, :size, :quantity, :price)
+  end
 end

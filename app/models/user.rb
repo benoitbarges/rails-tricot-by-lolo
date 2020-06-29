8ed2b79137b19f4d63c0f2dae3883377d61ab64c
benoit barges <benoitbarges@gmail.com>
2020-06-29 15:57:17 +0200

    user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_one :address
+  has_one :address, through: :orders
+  has_one :cart
   has_many :orders
 
   # Include default devise modules. Others available are:

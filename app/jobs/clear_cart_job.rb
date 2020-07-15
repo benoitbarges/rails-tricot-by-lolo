a6201081e418342e959e15fd8efe7e9f48f4ae16
benoit barges <benoitbarges@gmail.com>
2020-07-15 10:33:36 +0200

    switch picture on product show

--- app/jobs/clear_cart_job.rb
+++ app/jobs/clear_cart_job.rb
@@ -2,6 +2,6 @@ class ClearCartJob < ApplicationJob
   queue_as :default
 
   def perform
-    Cart.all.each { |cart| cart.order_products.where('created_at < ?', 10.seconds.ago).each(&:destroy) }
+    Cart.all.each { |cart| cart.order_products.where('created_at < ?', 10.minutes.ago).each(&:destroy) }
   end
 end
